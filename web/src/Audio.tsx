ecc84f061642f227531d5f0e9ccae9e574da45de
agdholo <agdholo@outlook.com>
2023-10-20 17:32:05 -0700

    add audio

--- web/src/Audio.tsx
+++ web/src/Audio.tsx
@@ -4,13 +4,12 @@ const SEND_INTERVAL = 1000; // 每秒发送一次
 
 const Audio: React.FC = () => {
   const [recording, setRecording] = useState<boolean>(false);
-  const audioContext = useRef<AudioContext | null>(null);
-  const processor = useRef<ScriptProcessorNode | null>(null);
+  const mediaRecorder = useRef<MediaRecorder | null>(null);
   const ws = useRef<WebSocket | null>(null);
-  const audioDataBuffer = useRef<Int16Array[]>([]); // 用于存储音频数据直到发送
+  const audioChunks = useRef<Blob[]>([]);
 
   useEffect(() => {
-    ws.current = new WebSocket("YOUR_WS_SERVER_URL");
+    ws.current = new WebSocket("ws://127.0.0.1:8000/ws");
 
     ws.current.onopen = () => {
       console.log("Connected to the WS server");
@@ -28,56 +27,39 @@ const Audio: React.FC = () => {
   }, []);
 
   const sendData = () => {
-    if (audioDataBuffer.current.length === 0) return;
+    if (audioChunks.current.length === 0) return;
 
-    // Calculate the total length of the concatenated buffer.
-    const totalLength = audioDataBuffer.current.reduce((acc, curr) => acc + curr.length, 0);
-
-    // Create a new buffer to hold all the data.
-    const concatenatedBuffer = new Int16Array(totalLength);
-
-    // Fill the new buffer with the data.
-    let offset = 0;
-    for (let buffer of audioDataBuffer.current) {
-      concatenatedBuffer.set(buffer, offset);
-      offset += buffer.length;
-    }
-
-    // Now you have a single Int16Array containing all the data.
+    const audioBlob = new Blob(audioChunks.current, {type: 'audio/webm'});
     if (ws.current && ws.current.readyState === WebSocket.OPEN) {
-      ws.current.send(concatenatedBuffer.buffer);
+      ws.current.send(audioBlob);
     }
 
-    audioDataBuffer.current = [];
+    audioChunks.current = [];
   };
-
 
   const startRecording = async () => {
     try {
       const stream = await navigator.mediaDevices.getUserMedia({audio: true});
 
-      audioContext.current = new AudioContext();
-      const source = audioContext.current.createMediaStreamSource(stream);
-      processor.current = audioContext.current.createScriptProcessor(4096, 1, 1);
+      mediaRecorder.current = new MediaRecorder(stream);
 
-      processor.current.onaudioprocess = (event) => {
-        if (!recording) return;
-
-        const channelData = event.inputBuffer.getChannelData(0);
-        const buffer = new Int16Array(channelData.length);
-        for (let i = 0; i < channelData.length; i++) {
-          buffer[i] = Math.min(1, channelData[i]) * 0x7FFF;
-        }
-        audioDataBuffer.current.push(buffer);
+      mediaRecorder.current.ondataavailable = event => {
+        audioChunks.current.push(event.data);
       };
 
-      source.connect(processor.current);
-      processor.current.connect(audioContext.current.destination);
+      mediaRecorder.current.onstop = sendData;
+
+      mediaRecorder.current.start();
 
       setRecording(true);
 
-      // 每秒发送一次
-      const interval = setInterval(sendData, SEND_INTERVAL);
+      const interval = setInterval(() => {
+        if (mediaRecorder.current && mediaRecorder.current.state === 'recording') {
+          mediaRecorder.current.stop();
+          mediaRecorder.current.start();
+        }
+      }, SEND_INTERVAL);
+
       return () => clearInterval(interval);
 
     } catch (err) {
@@ -86,12 +68,9 @@ const Audio: React.FC = () => {
   };
 
   const stopRecording = () => {
-    if (audioContext.current && processor.current) {
-      processor.current.disconnect(audioContext.current.destination);
-      audioContext.current.close();
-      setRecording(false);
+    if (mediaRecorder.current && mediaRecorder.current.state === 'recording') {
+      mediaRecorder.current.stop();
     }
-    sendData(); // 发送最后的数据
   };
 
   return (
