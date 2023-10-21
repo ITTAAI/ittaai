4e2e926ad85f6d9f20332b454f8591a5b4f245ff
PangYuanbo <147596230+PangYuanbo@users.noreply.github.com>
2023-10-21 10:32:38 -0700

    init

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import subprocess
 app = FastAPI()
 import os
 import openai
-openai.api_key = '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'
+openai.api_key = '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'
 @app.get("/")
 async def get():
     return HTMLResponse(html)
@@ -14,7 +14,10 @@ async def get():
 # ... 其他代码 ...
 
 @app.websocket("/ws")
+
 async def websocket_endpoint(websocket: WebSocket):
+    content = ''
+    count=0
     await websocket.accept()
     try:
         while True:
@@ -44,20 +47,71 @@ async def websocket_endpoint(websocket: WebSocket):
                         await websocket.send_text(transcription)
                         content+=transcription
                         count+=1
-                        if count==88 :
+                        if count == 88:
                             completion = openai.ChatCompletion.create(
                                 model="gpt-3.5-turbo",
                                 messages=[
-                                    {"role": "system", "content":content},
+                                    {"role": "system", "content": content},
                                     {"role": "user","content": "Please summarize the content I provide without changing the perspective and keep it within 300 words."}
-
                                 ]
                             )
-                            content=(completion.choices[0].message)
-                            count=0
+                            content = (completion.choices[0].message)
+                            count = 0
+                            # 写入更新后的内容到content.txt
+                            with open('content.txt', 'w') as file:
+                                file.write(content)
                 except Exception as e:
                     print(f"Error during transcription: {e}")
                     await websocket.send_text("Error during transcription. Please try again.")
 
     except WebSocketDisconnect:
         print("Client disconnected")
+@app.post("/submit-form")
+async def submit_form(q: str = Form(...), service: str = Form(...)):
+    content = ""
+    if os.path.exists('content.txt'):
+        with open('content.txt', 'r') as file:
+            content = file.read()
+
+    if service == "gpt":
+        # 如果用户选择了gpt，我们就调用GPT-3的服务
+        return await handle_gpt_service(q, content)
+    elif service == "claude":
+        # 如果用户选择了claude，我们就调用Claude的服务
+        # 注意: 你需要实现这个函数!
+        return await handle_claude_service(q, content)
+    else:
+        return {"error": "Invalid service selected"}
+
+# 处理GPT-3服务的函数
+async def handle_gpt_service(q: str, content: str):
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": content},
+                {"role": "user", "content": q}
+            ]
+        )
+        reply = response.choices[0].message
+        return {"GPT-3_response": reply}
+    except Exception as e:
+        return {"error": str(e)}
+
+# 需要实现的处理Claude服务的函数
+async def handle_claude_service(q: str, content: str):
+    # 构造到云函数的请求
+    url = "https://asvx1c.laf.dev/claude-chat"
+    params = {"question": q, "conversationId": content}  # 如果content是会话ID的话
+
+    # 发送异步HTTP GET请求到云函数
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, params=params)
+
+    # 检查请求是否成功
+    if response.status_code != 200:
+        raise HTTPException(status_code=response.status_code, detail="Claude API request failed.")
+
+    # 解析并返回结果
+    result = response.json()
+    return {"Claude_response": result.get("msg", "")}
